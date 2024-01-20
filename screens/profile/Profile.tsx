9bf9fe104da2d48cdde7785b5df0b83676d9c32d
3N14C <3n14c@vk.com>
2024-01-20 20:49:03 +0800

    updated profile

--- screens/profile/Profile.tsx
+++ screens/profile/Profile.tsx
@@ -1,11 +1,34 @@
+import { Button } from "@rneui/base";
 import { FC } from "react";
 import { Text } from "react-native";
 import { View } from "react-native";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import LoadingScreen from "../../components/LoadingScreen";
 
 export const Profile: FC = () => {
+  const { logout, user, loading } = useAuth();
+
+  const { navigate } = useNavigation();
+
+  console.log(user);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <View>
       <Text>Profile</Text>
+
+      <Button
+        onPress={() => {
+          logout();
+          // @ts-ignore
+          navigate("Login");
+        }}
+        title={"logout"}
+      />
     </View>
   );
 };

f6acd449a50f059f02b993a0f777cbf5bd28e78a
3N14C <3n14c@vk.com>
2024-01-21 02:38:59 +0800

    added modelPortal

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNativeRouter } from "./router/ReactNativeRouter";
 import { AuthContextProvider } from "./context/AuthContext";
 import Toast from "react-native-toast-message";
+import { ModalPortal } from "react-native-modals";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
         <ReactNativeRouter />
+        <ModalPortal />
         <Toast />
       </AuthContextProvider>
     </QueryClientProvider>

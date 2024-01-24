da19bbdc54c1ec865dda4c555024cbdf47e35d97
3N14C <3n14c@vk.com>
2024-01-25 05:12:08 +0800

    added coolectionID

--- appwrite/appwrite.constants.ts
+++ appwrite/appwrite.constants.ts
@@ -4,5 +4,5 @@ export const APPWRITE_DB = "65aac2be2a2639139d6a";
 export const COLLECTION_BRAND = "65aac2cd6638e6a5bc40";
 export const COLLECTION_OFFER = "65aac598134d7533acc9";
 export const COLLECTION_SIZE = "65aac6d61c3557bf0e39";
-export const COLLECTION_SNEAKER = "";
+export const COLLECTION_SNEAKER = "65aac574a266bdd3f587";
 export const COLLECTION_ORDER = "";
